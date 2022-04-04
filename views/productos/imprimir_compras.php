5a9a893f4975d3822994590cd52e2994458b6a36
Victor corio <65185738+coriovc@users.noreply.github.com>
2022-04-03 22:21:05 -0400

    Update imprimir_compras.php

--- views/productos/imprimir_compras.php
+++ views/productos/imprimir_compras.php
@@ -1,5 +1,5 @@
 <?php
-require_once "../../controladores/servicio.php";
+require_once "../../controladores/compra.php";
 ?>
 <script>window.print();</script> 
 </script>
@@ -10,14 +10,14 @@ require_once "../../controladores/servicio.php";
   include ('../../php/head.php');
   ?>
   <link rel="stylesheet" href="style-pdf.css" media="all" />
-    <title>Imprimir - Servicios </title>
+    <title>Imprimir - Compras </title>
   </head>
   <body>
     <header class="clearfix">
       <div id="logo">
         <img src="../../imagenes/logo/logo-dark-deli.png">
       </div>
-      <h1>Listado de Servicios</h1>
+      <h1>Listado de Compras</h1>
       <div id="project" class="clearfix">
         <div>D'licias</div>
         <div>Paseo Urdaneta,<br /> Local 6, Turmero</div>
@@ -29,39 +29,30 @@ require_once "../../controladores/servicio.php";
       <table>
         <thead>
           <tr>
-            <th>Nro</th>
-            <th>Nombre</th>
-            <th>Precio</th>
-            <th>Nombre de Categoria</th>
-            <th>Tiempo</th>
+            <th>Código Compra</th>
+            <th>Fecha Compra</th>
+            <th>Producto</th>
+            <th>Cantidad</th>
+            <th>Precio</th> 
           </tr>
         </thead>
         <tbody>
           <?php 
-          $resultados = listarServicio();
+          $resultados = listarCompra();
           foreach ($resultados as $key => $r){ ?>
-          <tr>
-            <td><?php echo $r['id']; ?></td>
-            <td><?php echo $r['nombre']; ?></td>
-            <td><?php echo $r['precio']; ?></td>
-            <td><?php echo $r['nombrecategoria']; ?></td>
-            <td><?php if ($r['tiempo']>60) {
-                        echo sprintf("%.2f",$r['tiempo']/60);
-                        echo ' H';
-                      }
-                      else{  
-                        echo $r['tiempo'];
-                        echo ' min';
-                      }?>
-
-            </td>
-          </tr>
+            <tr>
+              <td><?php echo $r['codigo_compra']; ?></td>
+              <td><?php echo date("d/m/Y",strtotime($r['fecha'])); ?></td>
+              <td><?php echo $r['nombreproducto']; ?></td>
+              <td><?php echo $r['cantidad']; ?> <?php echo $r['nombreunidad']; ?></td>
+              <td><?php echo $r['cantidad'] * $r['precio_c']  ; ?>$ </td> 
+            </tr>
           <?php } ?>
         </tbody>
       </table>
       <div id="notices">
         <div>NOTA:</div>
-        <div class="notice">Aqui se muestran todos los registros de las Servicios</div>
+        <div class="notice">Aqui se muestran todos los registros de las Compras</div>
       </div>
     </main>
     <footer>
